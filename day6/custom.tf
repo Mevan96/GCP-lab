0df125049cacb97b1279e6463e9114c012284b05
yauhen_mikhalchuk <yauhen_mikhalchuk@epam.com>
2021-07-15 01:26:56 +0300

    some copypast but still not work

--- day6/custom.tf
+++ day6/custom.tf
@@ -1,6 +1,8 @@
-resource "google_project_iam_custom_role" "custom-role" {
-  role_id     = "customRole"
+resource "google_project_iam_custom_role" "custom_role" {
+  role_id     = "custom_role"
   title       = "get"
+  project     = "gcp-lab-1-ym"
+  stage       = "ALPHA"
   description = "get files from all bucket and info abou VMs in project"
   permissions = ["storage.buckets.get", "compute.instances.get",
    "storage.buckets.list", "compute.instances.list"]
@@ -12,7 +14,7 @@ resource "google_service_account" "mikhalchuk-gke" {
 }
 
 resource "google_project_iam_member" "mikhalchuk-gke" {
-  role   = google_project_iam_custom_role.custom-role.id
+  role   = google_project_iam_custom_role.custom_role.id
   member = "serviceAccount:${google_service_account.mikhalchuk-gke.email}"
 }
 
@@ -21,17 +23,27 @@ resource "google_container_cluster" "ym-gke-cluster" {
   location                  = "us-central1"
   remove_default_node_pool  = true
   initial_node_count        = 1
+  workload_identity_config {
+    identity_namespace      = "gcp-lab-1-ym.svc.id.goog"
+  }
 }
 
-module "app-workload-identity" {
-  source      = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  name        = "ym-application"
-  project_id  = "gcp-lab-1-ym"
+module "my-app-workload-identity" {
+  source        = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  cluster_name  = google_container_cluster.ym-gke-cluster.name
+  name          = "sharayaugkeservice"
+  namespace     = kubernetes_namespace.nginxns.metadata.0.name
+  project_id    = "gcp-lab-1-ym"
+  roles         = ["projects/gcp-lab-1-ym/roles/custom_role"]
 }
 
-module "gke-workload-identity" {
-  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  use_existing_k8s_sa = true
-  name                = "mikhalchuk-gke"
-  project_id          = "gcp-lab-1-ym"
+resource "google_container_node_pool" "nginx-noge-pool" {
+  name          = "nginx-node-pool"
+  location      = "us-central1"
+  cluster       = google_container_cluster.ym-gke-cluster.name
+  node_count    = 1
+
+  node_config {
+    machine_type = "custom-1-1024"
+  }
 }
---
+++ day6/providers.tf
@@ -0,0 +1,13 @@
+data "google_client_config" "provider" {}
+
+provider "google" {
+  project = "gcp-lab-1-ym"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+provider "kubernetes" {
+  host = "https://${google_container_cluster.ym-gke-cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.ym-gke-cluster.master_auth[0].cluster_ca_certificate)
+}
