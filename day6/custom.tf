6c77db80d7e2a4271b104b5ed27991f2dcbc0fa3
yauhen_mikhalchuk <yauhen_mikhalchuk@epam.com>
2021-07-14 15:51:27 +0300

    day6 cluster done

--- day6/custom.tf
+++ day6/custom.tf
@@ -15,3 +15,23 @@ resource "google_project_iam_member" "mikhalchuk-gke" {
   role   = google_project_iam_custom_role.custom-role.id
   member = "serviceAccount:${google_service_account.mikhalchuk-gke.email}"
 }
+
+resource "google_container_cluster" "ym-gke-cluster" {
+  name                      = "ym-gke-cluster"
+  location                  = "us-central1"
+  remove_default_node_pool  = true
+  initial_node_count        = 1
+}
+
+module "app-workload-identity" {
+  source      = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  name        = "ym-application"
+  project_id  = "gcp-lab-1-ym"
+}
+
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "mikhalchuk-gke"
+  project_id          = "gcp-lab-1-ym"
+}
