aacd34d7fd356a39c031a15ed89f458ea5b8607b
yauhen_mikhalchuk <yauhen_mikhalchuk@epam.com>
2021-07-20 23:47:01 +0300

    proxy

--- ExitTask/balancers.tf
+++ ExitTask/balancers.tf
@@ -1,6 +1,46 @@
-# gclobal connector
-#resource "google_compute_global_address" "global-psconnect-ip" {
-#  name          = "global-psconnect-ip"
-#  project       = var.project
-#  address_type  = "EXTERNAL"
-#}
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "global-rule"
+  target     = google_compute_target_http_proxy.default.id
+  port_range = "80"
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name    = "test-proxy"
+  url_map = google_compute_url_map.default.id
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.default.id
+
+  host_rule {
+    hosts        = ["gcp-lab-1-ym.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_service.default.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_backend_service.default.id
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name        = "backend-service"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "http-health-check"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
